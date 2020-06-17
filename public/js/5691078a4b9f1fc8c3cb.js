1b869431fa36250c989e6d08cc16467f9533bb91
John Richardson <johnrich85@hotmail.com>
2020-06-17 08:06:53 +0100

    .

---
+++ public/js/5691078a4b9f1fc8c3cb.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./resources/js/pages/Register.vue~":
+/*!******************************************!*\
+  !*** ./resources/js/pages/Register.vue~ ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (1:0)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> <template>\\n|     <div class=\\\"container\\\">\\n|         <div class=\\\"row justify-content-center\\\">\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9wYWdlcy9SZWdpc3Rlci52dWV+LmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./resources/js/pages/Register.vue~\n");
+
+/***/ })
+
+}]);
---
+++ temp/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Open Sans', 'BlinkMacSystemFont'],
+        'serif': ['arvo', 'Georgia', 'Cambria'],
+        'display': ['bitter'],
+        'body': ['Poppins'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
